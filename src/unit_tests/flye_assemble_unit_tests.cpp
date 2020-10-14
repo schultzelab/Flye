59ce755595b9d2a06ff8167720417b26fdd89e07
Umesh Worlikar <umesh.worlikar@dzne.de>
2020-10-14 18:48:45 +0200

    Add unit test: Read header info

--- src/unit_tests/flye_assemble_unit_tests.cpp
+++ src/unit_tests/flye_assemble_unit_tests.cpp
@@ -1,8 +1,12 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <cstdlib>
 #include "doctest.h"
 #include "sequence_container.h"
 
-TEST_CASE("vectors can be sized and resized") {
-    std::vector<int> v(5);
-    CHECK(v.size() == 5);
+static auto data = std::string(std::getenv("TestData"));
+TEST_CASE("Read header info") {
+    SequenceContainer container;
+    container.loadFromFile(data + "/header-only.fasta", 0);
+
+    CHECK(&container != nullptr);
 }
